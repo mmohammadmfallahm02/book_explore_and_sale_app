715d9036ad344630545b0487e48250d8dcacc192
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-04-17 10:55:08 +0330

    first commit

---
+++ android/app/src/main/kotlin/com/example/book_explore_and_sale_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.book_explore_and_sale_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/theme/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/animation.dart';
+
+class MyColors{
+  MyColors._();
+
+  static const Color primaryColor=Color(0xffE49527);
+  static const Color primaryVariantColor=Color(0xff165C73);
+  static const Color secondaryColor=Color(0xff979797);
+  static const Color primaryTextColor=Color(0xff165C73);
+  static const Color secondaryTextColor=Color(0xff979797);
+  
+  
+}
