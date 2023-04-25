a5ecc41be16aeeb8407e7054565aded841e83160
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-04-25 20:01:02 +0330

    bookpage screen complited

--- lib/common/theme/colors.dart
+++ lib/common/theme/colors.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyColors {
-  MyColors._();
-
+abstract class MyColors {
   static const Color primaryColor = Color(0xffE49527);
   static const Color primaryVariantColor = Color(0xff165C73);
   static const Color secondaryColor = Color(0xff979797);
@@ -10,10 +8,16 @@ class MyColors {
   static const Color secondaryTextColor = Color(0xff979797);
 }
 
-class MyExclusiveColors {
-  MyExclusiveColors._();
+abstract class MyExclusiveColors {
   static const iconColor = Colors.black;
   static const searchBarColor = Color(0xffF6F5FA);
   static const inActiveDotIndicatorColor = Color(0xffD9D9D9);
   static const posterButtonTextColor = Color(0xffF89182);
+}
+
+abstract class MyGradient {
+  static const List<Color> bookPageDetailsGradient = [
+    Color(0xffffffff),
+    Color.fromARGB(120, 255, 255, 255),
+  ];
 }
--- lib/screens/book_page_screen.dart
+++ lib/screens/book_page_screen.dart
@@ -1,6 +1,7 @@
 import 'package:book_explore_and_sale_app/common/constants/constants.dart';
 import 'package:book_explore_and_sale_app/common/theme/colors.dart';
-import 'package:book_explore_and_sale_app/common/utils/utils_file.dart';
+import 'package:book_explore_and_sale_app/common/utils/utils.dart';
+
 import 'package:book_explore_and_sale_app/gen/assets.gen.dart';
 import 'package:book_explore_and_sale_app/models/book_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -131,22 +132,148 @@ class BookPageScreen extends StatelessWidget {
     final ThemeData themeData = Theme.of(context);
 
     return Scaffold(
+      floatingActionButton: SizedBox(
+          height: 35.h,
+          width: 149.w,
+          child: FloatingActionButton.extended(
+              shape: const BeveledRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              onPressed: () {},
+              label: Row(
+                children: [
+                  const Text('Add to Cart'),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  SvgPicture.asset(
+                    Assets.images.icons.cart.path,
+                    width: 15,
+                    height: 15,
+                    color: Colors.white,
+                  )
+                ],
+              ))),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: _buildCustomAppBar(context, themeData),
       body: Padding(
         padding: EdgeInsets.fromLTRB(
             MyDimens.bodyMargin, 31, MyDimens.bodyMargin, 0),
-        child: Column(children: [
-          _buildPosterWidget(),
-          const SizedBox(
-            height: 22,
-          ),
-          _buildMainInfoWidget(themeData),
-          const SizedBox(
-            height: 17,
-          ),
-          _buildStarAndReadBookWidget(themeData)
-        ]),
+        child: Stack(
+          children: [
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              _buildPosterWidget(),
+              const SizedBox(
+                height: 22,
+              ),
+              _buildMainInfoWidget(themeData),
+              const SizedBox(
+                height: 17,
+              ),
+              _buildStarAndReadBookWidget(themeData),
+              const SizedBox(
+                height: 32,
+              ),
+              const DetailsBookItemWidget(),
+              const SizedBox(
+                height: 17,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children:const [
+                       Text(
+                        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+                        style: TextStyle(height: 1.5),
+                      ),
+                      SizedBox(
+                        height: 130,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
+            Positioned(
+                bottom: 0,
+                right: 0,
+                left: 0,
+                child: Container(
+                  height: 141,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: MyGradient.bookPageDetailsGradient,
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter)),
+                ))
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class DetailsBookItemWidget extends StatefulWidget {
+  const DetailsBookItemWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<DetailsBookItemWidget> createState() => _DetailsBookItemWidgetState();
+}
+
+class _DetailsBookItemWidgetState extends State<DetailsBookItemWidget> {
+  int selectedItemIndex = 0;
+
+  Widget _buildListViewWidget(ThemeData themeData) {
+    return SizedBox(
+      height: 20,
+      child: ListView.builder(
+          itemCount: 3,
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            final List<String> list = ['About', 'Reviews', 'Author'];
+            final String item = list[index];
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedItemIndex = index;
+                });
+              },
+              child: Container(
+                decoration: index == selectedItemIndex
+                    ? const BoxDecoration(
+                        border: Border(
+                        bottom: BorderSide(
+                            width: 2.5, color: MyColors.primaryColor),
+                      ))
+                    : null,
+                padding: const EdgeInsets.only(left: 25, right: 20),
+                child: Text(
+                  item,
+                  style: themeData.textTheme.headline5,
+                ),
+              ),
+            );
+          }),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+    return Stack(
+      alignment: Alignment.bottomLeft,
+      children: [
+        const Divider(
+          thickness: 2,
+          color: MyExclusiveColors.searchBarColor,
+          height: 0,
+        ),
+        _buildListViewWidget(themeData),
+      ],
     );
   }
 }
